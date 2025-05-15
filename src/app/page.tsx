337c1e117caaa3d54bc12f6e8850244440c7a705
vgoyal <vgoyal@prepaynation.com>
2025-05-14 21:59:08 -0500

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
-import { CounterClientComponent } from "@/components/ClientComponent";
-import { ElectronCheck } from "@/components/ElectronCheck";
-import { ServerPokemonComponent } from "@/components/ServerComponent";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-around p-24 flex-row">
-      <ElectronCheck />
-
-      <ServerPokemonComponent />
-
-      <CounterClientComponent />
+    <main className="h-screen p-4 bg-black flex flex-col gap-y-4">
+      <div className="h-16 w-full border border-white bg-gray-200 rounded-2xl flex items-center justify-center">
+        Weather
+      </div>
+      <div className="grid grid-cols-2 gap-4 flex-1">
+        <div className="flex h-full w-full border border-white bg-gray-200 rounded-2xl items-center justify-center">
+          App
+        </div>
+        <div className="flex h-full w-full border border-white bg-gray-200 rounded-2xl items-center justify-center">
+          App
+        </div>
+        <div className="flex h-full w-full border border-white bg-gray-200 rounded-2xl items-center justify-center">
+          App
+        </div>
+        <div className="flex h-full w-full border border-white bg-gray-200 rounded-2xl items-center justify-center">
+          App
+        </div>
+      </div>
     </main>
   );
 }
+
